c0f5d456e4205232376ef2e949d5f210867fbd76
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-11-15 07:31:15 -0500

    Add deployment workflow

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# https://vitejs.dev/guide/static-deploy.html#github-pages
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:tg2648/frequent-transactions.git main:gh-pages
+
+cd -
