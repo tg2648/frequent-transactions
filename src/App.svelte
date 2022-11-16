d2e65b0eb2945fa51f40d2584064e96c918a75b9
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-11-15 21:19:38 -0500

    Add YNAB auth

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,62 @@
 <script>
+  import { onMount } from "svelte";
+  import * as ynab from "ynab";
+
+  // Import our config for YNAB
+  import config from "./ynab_config.json";
+
+  let token;
+  let ynabApi;
+
+  onMount(() => {
+    token = findYNABToken();
+    if (token) {
+      ynabApi = new ynab.api(token);
+    }
+  });
+
+  function authorizeWithYNAB() {
+    const uri = `https://app.youneedabudget.com/oauth/authorize?client_id=${config.clientId}&redirect_uri=${config.redirectUri}&response_type=token`;
+    location.replace(uri);
+  }
+
+  function findYNABToken() {
+    let token = null;
+    const search = window.location.hash
+      .substring(1)
+      .replace(/&/g, '","')
+      .replace(/=/g, '":"');
+
+    if (search && search !== "") {
+      // Try to get access_token from the hash returned by OAuth
+      const params = JSON.parse('{"' + search + '"}', function (key, value) {
+        return key === "" ? value : decodeURIComponent(value);
+      });
+
+      token = params.access_token;
+      sessionStorage.setItem("ynab_access_token", token);
+      window.location.hash = "";
+    } else {
+      // Otherwise try sessionStorage
+      token = sessionStorage.getItem("ynab_access_token");
+    }
+
+    return token;
+  }
 </script>
 
 <main>
   <h1>Frequent Transactions for YNAB</h1>
 
-  <h2>Privacy Policy</h2>
+  <p>Token: {token}</p>
+  <button on:click|preventDefault={authorizeWithYNAB}
+    >Authorize with YNAB</button
+  >
+
+  <h2 id="privacy">Privacy Policy</h2>
   <p>
     This website does not store any information from you or your YNAB account.
-    All data retrieved from the YNAB API is stored only in your browser and is never
-    transmitted to any other location or third-party.
+    All data retrieved from the YNAB API is stored only in your browser and is
+    never transmitted to any other location or third-party.
   </p>
 </main>
