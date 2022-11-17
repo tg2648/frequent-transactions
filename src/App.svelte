bc2c4a6af5bade09e7ec7f680b8a93eb57c41232
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-11-17 17:51:53 -0500

    Add listing of budget names

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,20 @@
-<svelte:head>
-  <script src="ynab.js" async></script>
-</svelte:head>
-
 <script>
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
+  import * as ynab from "ynab";
 
   // Import our config for YNAB
-  import { config } from './ynab_config'
+  import { config } from "./ynab_config";
 
   let token;
-  let ynabApi;
-  let ynab = window.ynab;
+  let api = new ynab.api("temp");
+  let budgets = [];
+  let loading;
 
   onMount(() => {
     token = findYNABToken();
     if (token) {
-      //@ts-ignore
-      ynabApi = new ynab.api(token);
+      api = new ynab.api(token);
+      getBudgets();
     }
   });
 
@@ -32,18 +30,18 @@
       .replace(/&/g, '","')
       .replace(/=/g, '":"');
 
-    if (search && search !== '') {
+    if (search && search !== "") {
       // Try to get access_token from the hash returned by OAuth
       const params = JSON.parse('{"' + search + '"}', function (key, value) {
-        return key === '' ? value : decodeURIComponent(value);
+        return key === "" ? value : decodeURIComponent(value);
       });
 
       token = params.access_token;
-      sessionStorage.setItem('ynab_access_token', token);
-      window.location.hash = '';
+      sessionStorage.setItem("ynab_access_token", token);
+      window.location.hash = "";
     } else {
       // Otherwise try sessionStorage
-      token = sessionStorage.getItem('ynab_access_token');
+      token = sessionStorage.getItem("ynab_access_token");
     }
 
     return token;
@@ -53,23 +51,43 @@
     token = null;
     sessionStorage.clear();
   }
+
+  function getBudgets() {
+    loading = true;
+    api.budgets
+      .getBudgets()
+      .then((res) => {
+        budgets = res.data.budgets;
+      })
+      .catch((err) => {
+        console.error(err.error.detail);
+      })
+      .finally(() => {
+        loading = false;
+      });
+  }
 </script>
 
 <main>
   <h1>Frequent Transactions for YNAB</h1>
 
-  {#if token}
-    Logged in
-    <button on:click|preventDefault={logout}>
-      Logout
-    </button>
+  {#if loading}
+    <p>Loading...</p>
+  {:else if token}
+    {#each budgets as budget (budget.id)}
+      <p>{budget.name}</p>
+    {/each}
+    <p>
+      Logged in
+      <button on:click|preventDefault={logout}> Logout </button>
+    </p>
   {:else}
     <button on:click|preventDefault={authorizeWithYNAB}>
       Authorize with YNAB
     </button>
   {/if}
 
-  <h2 id='privacy'>Privacy Policy</h2>
+  <h2 id="privacy">Privacy Policy</h2>
   <p>
     This website does not store any information from you or your YNAB account.
     All data retrieved from the YNAB API is stored only in your browser and is
