7e8a5affb6591211e15266e38b2666ff52a00796
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-12-30 19:39:31 -0500

    Remove old functions

--- netlify/functions/test-cookie.js
+++
@@ -1,8 +0,0 @@
-const cookie = require("cookie");
-
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: event.headers.cookie }),
-  };
-};
