06ef55a80b3caab79da12097daa6c3aba3acfd5f
tg2648 <45052145+tg2648@users.noreply.github.com>
2023-07-26 16:49:49 -0400

    Update footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -8,10 +8,10 @@
     Frequent Transactions for YNAB does not collect any personal information and
     does not store any information from you or your YNAB account. All data
     retrieved from YNAB is stored only in your browser and is never transmitted
-    to any other location or third-party. This project is under
-    <a href="https://github.com/tg2648/frequent-transactions/blob/main/LICENSE">
-      MIT License
-    </a>.
+    to any other location or third-party. This project is open source under the <a
+      href="https://github.com/tg2648/frequent-transactions/blob/main/LICENSE"
+      >MIT License</a
+    >.
   </p>
   <div class="d-flex justify-content-center fs-4 mt-5">
     <GithubLink />
